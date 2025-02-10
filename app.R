3c7b5e27955906bb917ea32ab3c766eaf0b7bc8e
Cesar Ivan <calvites1990@gmail.com>
2025-02-10 10:48:50 +0100

    best

--- app.R
+++ app.R
@@ -1,9 +1,5 @@
 # ******************************************************************************
 # This is CSF-Ind web-based app 
 # ******************************************************************************
-#  repository GitHub
-source("https://raw.githubusercontent.com/Cesarito2021/OwnLibraries/main/base.R")
-
-
 # Run the application 
-shinyApp(ui, server, options = list(height = 1300))
+shinyApp(ui = ui, server = server, options = list(height = 1300))
