0d164d2ae10e1b4d285279456ca184300ea080d1
Cesar Ivan <calvites1990@gmail.com>
2025-02-10 10:32:49 +0100

    best

--- app.R
+++ app.R
@@ -1,11 +1,9 @@
 # ******************************************************************************
 # This is CSF-Ind web-based app 
 # ******************************************************************************
-# Usa source() per caricare il codice base.R dal tuo repository GitHub
+#  repository GitHub
 source("https://raw.githubusercontent.com/Cesarito2021/OwnLibraries/main/base.R")
 
-# Ora puoi utilizzare la variabile 'ui' e 'server' definiti in base.R
-shinyApp(ui, server, options = list(height = 1300))
 
 # Run the application 
 shinyApp(ui, server, options = list(height = 1300))
